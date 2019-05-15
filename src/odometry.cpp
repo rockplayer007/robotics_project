3ae014e509d74518966ea78ed17a93d8f7786382
rockplayer007 <maildiroland@yahoo.it>
2019-05-15 18:08:35 +0200

    reading from the bag works!

--- src/odometry.cpp
+++ src/odometry.cpp
@@ -1,24 +1,53 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
 #include "std_msgs/Float64.h"
-//rostopic echo /speed_L -b bag_1.bag -p > file.csv
+
+#include "project/floatStamped.h"
+
+#include "geometry_msgs/Vector3Stamped.h"
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+
+#include <message_filters/sync_policies/exact_time.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
+
+
+void callback(const project::floatStamped::ConstPtr& msg1, const project::floatStamped::ConstPtr& msg2)
+{
+  ROS_INFO ("Received two messages: (%f) and (%f)", msg1->data, msg2->data);
+}
 
 void chatterCallback(const std_msgs::Float64::ConstPtr& msg)
 {
   ROS_INFO("Speed LEFT: [%f]", msg->data);
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
+  ros::init(argc, argv, "odometry");
 
-  ros::init(argc, argv, "odometer");
+  ros::NodeHandle n; 
 
-  ros::NodeHandle n;
+  message_filters::Subscriber<project::floatStamped> sub1(n, "speedL_stamped", 1);
+  message_filters::Subscriber<project::floatStamped> sub2(n, "speedR_stamped", 1);
 
-  ros::Subscriber sub = n.subscribe("speed_L", 1000, chatterCallback);
+  //ros::Subscriber sub = n.subscribe("speed_L", 1000, chatterCallback);
+
+  //typedef message_filters::sync_policies::ExactTime<geometry_msgs::Vector3Stamped, geometry_msgs::Vector3Stamped> MySyncPolicy;
+  typedef message_filters::sync_policies::ApproximateTime<project::floatStamped, project::floatStamped> MySyncPolicy;
+  
+  
+  message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(10), sub1, sub2);
+  //ROS_INFO ("Received two messages");
+  sync.registerCallback(boost::bind(&callback, _1, _2));
+
+
+  //message_filters::TimeSynchronizer<project::floatStamped, project::floatStamped> sync(sub1, sub2, 1000);
+  //sync.registerCallback(boost::bind(&callback, _1, _2));
+  //ROS_INFO ("Received two messages");
 
   ros::spin();
 
-
   return 0;
 }
+
